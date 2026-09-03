60afdd44e5262731c5c252a0c30386570a0d375e
Mosh <mosh_shood@hotmail.com>
2026-09-03 18:46:26 +0100

    fix: IAM eks cluster policy

--- platform/identity/github-oidc/main.tf
+++ platform/identity/github-oidc/main.tf
@@ -846,3 +846,8 @@ resource "aws_iam_role" "eks_cluster" {
   assume_role_policy = data.aws_iam_policy_document.eks_cluster_trust.json
   description        = "EKS cluster role allows control plane to manage AWS resources"
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
